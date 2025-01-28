2eee86475d3549d999a308ff2a059f683cb11686
seunghyeonKang <number42isuser@gmail.com>
2025-01-28 17:55:59 +0900

    ✨: smoking-area 페이지 - 상단 정보 컴포넌트 작업

---
+++ src/components/smoking-area/default/SmokingAreaInfo.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Star } from "lucide-react"
+import UpdateBtn from "./UpdateBtn"
+
+export default function SmokingAreaInfo({ smoking_name, region }) {
+	return (
+		<>
+			{/* text-[13px] font-semibold text-[#000] */}
+
+			<div className="mx-[20px] flex flex-col gap-[6px] py-[16px] text-[#252525]">
+				<p className="font-regular text-[12px] text-[#b5b5b5]">
+					내 위치에서 100m
+				</p>
+				<div className="mt-[-3px] flex items-center justify-between">
+					<h2 className="text-[16px] font-bold">{smoking_name}</h2>
+					<UpdateBtn />
+				</div>
+
+				<p className="font-regular text-[10px]">{region}</p>
+				<div className="flex items-center gap-[2px]">
+					<Star className="h-3 w-3 fill-[#4517FF] text-[#4517FF]" />
+					<span className="text-[10px] font-bold">4.3(3) ꞏ 저장 30</span>
+				</div>
+			</div>
+		</>
+	)
+}
--- src/pages/smoking-area/SmokingAreaPage.jsx
+++ src/pages/smoking-area/SmokingAreaPage.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Topbar from "@/components/smoking-area/default/Topbar"
 import BackgroundImg from "@/components/smoking-area/default/BackgroundImg"
+import SmokingAreaInfo from "@/components/smoking-area/default/SmokingAreaInfo"
+
 import DummyPng from "@/assets/dummy/SM_01_img1.png" // imageUrl
 
 export default function SmokingAreaPage() {
 	const [bgImg, setBgImg] = useState(DummyPng) // imageUrl
+	const [smokingArea, setSmokingArea] = useState({})
+
+	useEffect(() => {
+		const fetchSmokingArea = async () => {
+			try {
+				const response = await fetch(`http://localhost:3001/smokingAreas`)
+				const data = await response.json()
+				console.log(data[0])
+
+				setSmokingArea(data[0] || {})
+			} catch (error) {
+				console.error("흡연 구역 데이터를 가져오지 못했습니다.", error)
+			}
+		}
+
+		fetchSmokingArea()
+	}, [])
 
 	return (
-		<>
+		<div className="bg-white">
 			<Topbar />
+			{/* 이미지 연결 예정 */}
 			{bgImg && <BackgroundImg bgImg={bgImg} />}
 
-			<p>smoking-area([SM_01] 흡연 구역 정보)</p>
-		</>
+			{/* api 추후 수정 예정 */}
+			<SmokingAreaInfo {...smokingArea} />
+		</div>
 	)
 }
