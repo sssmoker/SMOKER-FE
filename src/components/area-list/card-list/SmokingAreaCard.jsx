638bd8ca28029bbd60da454b403f530a25c6444d
seunghyeonKang <number42isuser@gmail.com>
2025-01-31 21:45:58 +0900

    ✨: list 페이지 - 페이지 이동 추가

--- src/components/area-list/card-list/SmokingAreaCard.jsx
+++ src/components/area-list/card-list/SmokingAreaCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Bookmark, Star } from "lucide-react"
 import ImgCard from "./ImgCard"
+import { useNavigate } from "react-router-dom"
 
 export default function SmokingAreaCard({
 	title,
@@ -10,45 +11,54 @@ export default function SmokingAreaCard({
 	bookmarkCount,
 	distance,
 	img,
+	smoking_id,
 }) {
+	const navigate = useNavigate()
 	const [bookmarked, setBookmarked] = useState(isBookmarked)
 
-	const handleBookmark = () => {
+	const toggleBookmark = () => {
 		setBookmarked(!bookmarked)
+	}
+
+	const handleMoveToSmokingArea = () => {
+		navigate(`/list/smoking-area?id=${smoking_id}`)
 	}
 
 	return (
 		<>
 			<li className="flex w-full gap-[16px] border-b-[0.5px] border-[#e6e6e6] px-5 py-4">
 				{img && <ImgCard Img={img} />}
-				<div className="flex w-full flex-col gap-y-1">
-					<div className="flex justify-between">
+				<div className="flex w-full justify-between">
+					<div
+						onClick={handleMoveToSmokingArea}
+						className="flex w-full flex-col gap-y-1"
+					>
 						<p className="text-[13px] font-semibold text-[#000]">{title}</p>
-						<Bookmark
-							onClick={handleBookmark}
-							className={`h-5 w-5 text-[#252525] ${
-								bookmarked ? "fill-[#252525]" : "fill-none"
-							}`}
-						/>
-					</div>
 
-					<div className="flex items-center gap-0.5">
-						<Star className="h-3 w-3 fill-[#4517FF] text-[#4517FF]" />
-						<p className="text-[10px] font-normal text-[#252525]">
-							{`${rating}(${ratingCount}) ꞏ 저장 ${bookmarkCount}`}
-						</p>
-						{!img && (
+						<div className="flex items-center gap-0.5">
+							<Star className="h-3 w-3 fill-[#4517FF] text-[#4517FF]" />
+							<p className="text-[10px] font-normal text-[#252525]">
+								{`${rating}(${ratingCount}) ꞏ 저장 ${bookmarkCount}`}
+							</p>
+							{!img && (
+								<p className="ml-1 text-[8px] font-normal text-[#B5B5B5]">
+									{`내 위치에서 ${distance}`}
+								</p>
+							)}
+						</div>
+						{img && (
 							<p className="ml-1 text-[8px] font-normal text-[#B5B5B5]">
 								{`내 위치에서 ${distance}`}
 							</p>
 						)}
 					</div>
 
-					{img && (
-						<p className="ml-1 text-[8px] font-normal text-[#B5B5B5]">
-							{`내 위치에서 ${distance}`}
-						</p>
-					)}
+					<Bookmark
+						onClick={toggleBookmark}
+						className={`h-5 w-5 text-[#252525] ${
+							bookmarked ? "fill-[#252525]" : "fill-none"
+						}`}
+					/>
 				</div>
 			</li>
 		</>
