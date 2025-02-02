193b22aea47379f5ed43bf92d9993511af51056b
seunghyeonKang <number42isuser@gmail.com>
2025-02-02 14:06:51 +0900

    ✨: list 페이지 예외사항 처리 추가

--- src/components/area-list/card-list/SmokingAreaList.jsx
+++ src/components/area-list/card-list/SmokingAreaList.jsx
@@ -4,10 +4,17 @@ import SmokingAreaCard from "./SmokingAreaCard"
 export default function SmokingAreaList({ smokingAreas = [] }) {
 	return (
 		<>
-			{/* key값 id로 수정하자 */}
-			{smokingAreas.map((data, i) => (
-				<SmokingAreaCard key={i} {...data} />
-			))}
+			{smokingAreas.length ? (
+				smokingAreas.map((data) => (
+					<SmokingAreaCard key={data.smoking_id} {...data} />
+				))
+			) : (
+				<div className="mt-[10vh] flex flex-col items-center">
+					<p className="text-[15px] font-bold text-[#555]">
+						근처에 흡연 구역이 없습니다.
+					</p>
+				</div>
+			)}
 		</>
 	)
 }
