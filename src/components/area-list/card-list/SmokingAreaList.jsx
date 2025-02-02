a18064cf37a85d632cf08fcd91b8c8a41225de98
seunghyeonKang <number42isuser@gmail.com>
2025-02-02 12:34:25 +0900

    🐛: eslint error 수정

--- src/components/area-list/card-list/SmokingAreaList.jsx
+++ src/components/area-list/card-list/SmokingAreaList.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import SmokingAreaCard from "./SmokingAreaCard"
 
-export default function SmokingAreaList({ smokingAreas }) {
+export default function SmokingAreaList({ smokingAreas = [] }) {
 	return (
 		<>
 			{/* key값 id로 수정하자 */}
--- src/pages/smoking-area/SmokingAreaReviewPage.jsx
+++ src/pages/smoking-area/SmokingAreaReviewPage.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react"
+import React from "react"
 import ReviewCard from "@/components/smoking-area/review/ReviewCard"
 import StarRatingStatistics from "@/components/smoking-area/review/StarRatingStatistics"
 
 export default function SmokingAreaReviewPage({
 	starRatingData,
-	reviewListData,
+	reviewListData = [],
 }) {
-	const [reviews, setReviews] = useState(reviewListData)
-
 	return (
 		<div className="pb-[20vh]">
-			<StarRatingStatistics {...starRatingData} />
+			{reviewListData.length ? (
+				<>
+					<StarRatingStatistics {...starRatingData} />
 
-			{reviews.length ? (
-				reviews.map((review) => <ReviewCard key={review.id} {...review} />)
+					{reviewListData.map((review) => (
+						<ReviewCard key={review.id} {...review} />
+					))}
+				</>
 			) : (
 				<p>리뷰가 없습니다.</p>
 			)}
