bd81c2ee5bff605a71980a993f3bea476937f95c
hywznn <choihj0510@naver.com>
2025-02-10 20:35:34 +0900

    🔧: page구조에 맞게 route 재정리

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,122 +1,145 @@
 import React from "react"
 import { createBrowserRouter, Outlet } from "react-router-dom"
 import RootLayout from "@/layouts/root-layout"
-import HomePage from "@/pages/HomePage"
-import ListPage from "@/pages/ListPage"
-import SmokingAreaPage from "@/pages/smoking-area/SmokingAreaPage"
-import SmokingAreaUpdatePage from "@/pages/smoking-area/SmokingAreaUpdatePage"
+import HomePage from "@/pages/main/HomePage"
+import ListPage from "@/pages/main/ListPage"
+import SmokingAreaDetailPage from "@/pages/smoking-area/SmokingAreaDetailPage"
+import SmokingAreaHistoryPage from "@/pages/smoking-area/SmokingAreaReviewPage"
 import WritingReviewPage from "@/pages/smoking-area/WritingReviewPage"
+import SmokingAreaReviewPage from "@/pages/update-smoking-area/SmokingAreaHistoryPage"
+import SmokingAreaUpdatePage from "@/pages/update-smoking-area/SmokingAreaUpdatePage"
 import AddSmokingAreaPage from "@/pages/add-smoking-area/AddSmokingAreaPage"
 import AddSmokingAreaNamePage from "@/pages/add-smoking-area/AddSmokingAreaNamePage"
-import MyPage from "@/pages/auth/MyPage"
-import MemberInfoPage from "@/pages/auth/MemberInfoPage"
-import LoginPage from "@/pages/auth/LoginPage"
-import SavedSmokingAreasPage from "@/pages/favorites/SavedSmokingAreasPage"
 import AddSmokingAreaImagePage from "@/pages/add-smoking-area/AddSmokingAreaImagePage"
-import SmokingAreaHistoryPage from "./pages/smoking-area/SmokingAreaHistoryPage"
-// "list" 경로와 하위 페이지들
+import SavedSmokingAreasPage from "@/pages/favorites/SavedSmokingAreasPage"
+import MyPage from "@/pages/my-page-list/MyPage"
+import MemberInfoPage from "@/pages/my-page-list/MemberInfoPage"
+import MemberReviewsPage from "@/pages/my-page-list/MemberReviewsPage"
+import LoginPage from "@/pages/main/LoginPage"
+
 const listRoutes = [
 	{
-		index: true, // 기본 ListPage
+		index: true,
 		element: <ListPage />,
 	},
 	{
-		path: "smoking-area",
-		element: <Outlet />, // 하위 smoking-area 페이지들
+		path: ":smokingAreaId",
+		element: <Outlet />,
 		children: [
 			{
-				index: true,
-				element: <SmokingAreaPage />, // 상세 정보 및 후기 페이지
+				path: "info",
+				element: <SmokingAreaDetailPage />,
+			},
+			{
+				path: "reviews",
+				element: <SmokingAreaHistoryPage />,
+			},
+			{
+				path: "add-review",
+				element: <WritingReviewPage />,
 			},
 			{
 				path: "history",
-				element: <SmokingAreaHistoryPage />, // 히스토리 페이지
+				element: <SmokingAreaReviewPage />,
 			},
 			{
-				path: "update",
-				element: <SmokingAreaUpdatePage />, // 흡연 구역 업데이트 페이지
-			},
-			{
-				path: "writing-review",
-				element: <WritingReviewPage />, // 리뷰 작성 페이지
+				path: "history/update",
+				element: <SmokingAreaUpdatePage />,
 			},
 		],
 	},
 ]
 
-// "my-page" 경로와 하위 페이지들
-const myPageRoutes = [
-	{
-		index: true,
-		element: <MyPage />, // 기본 마이 페이지
-	},
-	{
-		path: "login",
-		element: <LoginPage />, // 로그인 페이지
-	},
-	{
-		path: "info",
-		element: <MemberInfoPage />, // 회원 정보 페이지
-	},
-]
-
-// "add-smoking-area" 경로와 하위 페이지들
 const addSmokingAreaRoutes = [
 	{
 		path: "location",
-		element: <AddSmokingAreaPage />, // 위치 설정 페이지
+		element: <AddSmokingAreaPage />,
 	},
 	{
 		path: "name",
-		element: <AddSmokingAreaNamePage />, // 이름 입력 페이지
+		element: <AddSmokingAreaNamePage />,
 	},
 	{
 		path: "image",
-		element: <AddSmokingAreaImagePage />, // 이름 입력 페이지
+		element: <AddSmokingAreaImagePage />,
 	},
 ]
 
-// "favorites" 경로 추가
+const myPageRoutes = [
+	{
+		index: true,
+		element: <MyPage />,
+	},
+	{
+		path: "login",
+		element: <LoginPage />,
+	},
+	{
+		path: ":memberId",
+		element: <Outlet />,
+		children: [
+			{
+				path: "info",
+				element: <MemberInfoPage />,
+			},
+			{
+				path: "reviews",
+				element: <MemberReviewsPage />,
+			},
+			{
+				path: "name",
+				element: <MyPage />,
+			},
+		],
+	},
+	{
+		path: "questions",
+		element: <MyPage />,
+	},
+	{
+		path: "notice",
+		element: <MyPage />,
+	},
+]
+
 const favoritesRoutes = [
 	{
 		index: true,
-		element: <SavedSmokingAreasPage />, // 저장된 흡연 구역 페이지
+		element: <SavedSmokingAreasPage />,
 	},
 ]
 
-// 전체 루트와 경로 정의
 const routes = [
 	{
 		index: true,
-		element: <HomePage />, // 홈 페이지
+		element: <HomePage />,
 	},
 	{
 		path: "list",
 		element: <Outlet />,
-		children: listRoutes, // "list"와 하위 경로
+		children: listRoutes,
 	},
 	{
 		path: "add-smoking-area",
 		element: <Outlet />,
-		children: addSmokingAreaRoutes, // "add-smoking-area"와 하위 경로
+		children: addSmokingAreaRoutes,
 	},
 	{
 		path: "my-page",
 		element: <Outlet />,
-		children: myPageRoutes, // "my-page"와 하위 경로
+		children: myPageRoutes,
 	},
 	{
 		path: "favorites",
 		element: <Outlet />,
-		children: favoritesRoutes, // "favorites" 경로와 하위 경로
+		children: favoritesRoutes,
 	},
 ]
 
-// createBrowserRouter로 전체 라우팅 설정
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <RootLayout />, // 레이아웃 컴포넌트
+		element: <RootLayout />,
 		children: routes,
 	},
 ])
--- src/layouts/root-layout.jsx
+++ src/layouts/root-layout.jsx
@@ -1,12 +1,19 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import Navbar from "@/components/common/Navbar"
 
 const RootLayout = () => {
+	const navigate = useNavigate()
+	const location = useLocation()
+
 	const handleRefresh = () => {
-		// 새로고침 로직
-		console.log("Refreshing HomePage...")
-		window.location.reload() // 페이지 새로고침
+		if (location.pathname === "/") {
+			console.log("Refreshing HomePage...")
+			window.location.reload() // 홈 페이지에서만 강제 새로고침
+		} else {
+			console.log("Navigating to HomePage...")
+			navigate("/", { replace: true })
+		}
 	}
 
 	return (
